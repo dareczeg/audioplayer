fb1fad87ccb019a01e512313eeb0ea9af0cb88ba
dareczeg <32062687+dareczeg@users.noreply.github.com>
2017-10-20 21:37:00 +0200

    Add files via upload

---
+++ Programmuzyczny/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace Programmuzyczny
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.lbNama = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lbSumber = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.Chocolate;
+            this.progressBar1.Location = new System.Drawing.Point(25, 165);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(462, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 5;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Font = new System.Drawing.Font("Lewisham", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label1.Location = new System.Drawing.Point(35, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 6;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "mp3";
+            this.openFileDialog1.Filter = "Plik mp3 | *mp3";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Programmuzyczny.Properties.Resources.play;
+            this.pictureBox1.Location = new System.Drawing.Point(225, 77);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(52, 48);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::Programmuzyczny.Properties.Resources.pause;
+            this.pictureBox2.Location = new System.Drawing.Point(126, 74);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(49, 50);
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = global::Programmuzyczny.Properties.Resources.staph;
+            this.pictureBox3.Location = new System.Drawing.Point(325, 78);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(46, 45);
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.Image = global::Programmuzyczny.Properties.Resources.backward;
+            this.pictureBox4.Location = new System.Drawing.Point(32, 77);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(52, 50);
+            this.pictureBox4.TabIndex = 10;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox5.Image = global::Programmuzyczny.Properties.Resources.forward;
+            this.pictureBox5.Location = new System.Drawing.Point(427, 81);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox5.TabIndex = 11;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // lbNama
+            // 
+            this.lbNama.FormattingEnabled = true;
+            this.lbNama.Location = new System.Drawing.Point(25, 194);
+            this.lbNama.Name = "lbNama";
+            this.lbNama.Size = new System.Drawing.Size(462, 407);
+            this.lbNama.TabIndex = 12;
+            this.lbNama.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.lbNama.DoubleClick += new System.EventHandler(this.lbNama_DoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(38, 563);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Add to playlist";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(350, 563);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Remove from playlist";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lbSumber
+            // 
+            this.lbSumber.FormattingEnabled = true;
+            this.lbSumber.Location = new System.Drawing.Point(156, 271);
+            this.lbSumber.Name = "lbSumber";
+            this.lbSumber.Size = new System.Drawing.Size(120, 95);
+            this.lbSumber.TabIndex = 15;
+            this.lbSumber.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Programmuzyczny.Properties.Resources.bg;
+            this.ClientSize = new System.Drawing.Size(521, 192);
+            this.Controls.Add(this.lbSumber);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbNama);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Program muzyczny";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.ListBox lbNama;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox lbSumber;
+    }
+}
+
---
+++ Programmuzyczny/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WMPLib;
+using System.IO;
+using System.Runtime.InteropServices;
+using MediaPlayer;
+
+namespace Programmuzyczny
+{
+    public partial class Form1 : Form
+    {
+
+        WindowsMediaPlayer mjuzik = new WindowsMediaPlayer();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            if (progressBar1.Value != 4000)
+            {
+                progressBar1.Value++;
+            }
+            else
+            {
+                timer1.Stop();
+            }
+
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.FileName == "")
+                try
+                {
+                    if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                    {
+                        mjuzik.URL = openFileDialog1.FileName.ToString();
+                        mjuzik.URL = openFileDialog1.FileName;
+                        label1.Text = "Current track - " + Path.GetFileName(mjuzik.URL).ToString();
+
+                        mjuzik.controls.play();
+                        timer1.Enabled = true;
+
+                        timer1.Start();
+                        timer1.Interval = 50;
+                        progressBar1.Maximum = 4000;
+                        timer1.Tick += new EventHandler(timer1_Tick);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            else
+                try
+                {
+                    mjuzik.controls.play();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            mjuzik.controls.currentPosition -= 10;
+            mjuzik.controls.previous();
+        }
+
+
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            mjuzik.controls.currentPosition += 10;
+            mjuzik.controls.next();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            mjuzik.controls.pause();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            mjuzik.controls.stop();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lbNama.SelectedIndex = lbNama.SelectedIndex;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lbNama.Items.Add(openFileDialog1.SafeFileName);
+            lbSumber.Items.Add(openFileDialog1.URL);
+        }
+
+        private void lbNama_DoubleClick(object sender, EventArgs e)
+        {
+            mjuzik.URL= lbSumber.SelectedItem;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lbSumber.Items.Remove(lbSumber.SelectedItem);
+            lbNama.Items.Remove(lbNama.SelectedItem);
+        }
+        
+    }
+}
